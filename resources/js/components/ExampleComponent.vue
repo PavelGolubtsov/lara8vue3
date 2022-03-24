8be789850ce8472b24980a1e8978fcee65651691
PavelGolubtsov <p.golubcov@mail.ru>
2022-03-24 03:18:16 +0300

    setup lara8vue3

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>
+            I'm an example component.
+        </h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lara8vue3</title>
+</head>
+<body>
+    <h1>Laravel 8 and vue 3</h1>
+    <div id="app">
+        <example-component></example-component>
+    </div>
+
+<script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
